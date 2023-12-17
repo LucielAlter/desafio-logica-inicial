1d6279c3c09b96b83c0999e24fbae77cb1160d3e
LucielAlter <klabidgames@gmail.com>
2023-12-17 13:38:25 -0300

    meu primeiro desafio

---
+++ desafio.js
@@ -0,0 +1,23 @@
+// heroi 
+let nome = "Gustavão2000"
+let XP = 9001
+let nivel = ""
+if ( XP <=1000 ){
+ nivel = "Ferro"
+}else if((XP >=1001)&&(XP <=2000)){
+nivel = "Bronze"
+}else if((XP >=2001)&&(XP <=5000)){
+nivel = "Prata"
+}
+else if((XP >=6001)&&(XP <=7000)){
+nivel = "Ouro"
+}else if((XP >=7001)&&(XP <=8000)){
+nivel = "Platina"
+}else if((XP >=8001)&&(XP <=9000)){
+nivel = "Ascendente"
+}else if((XP >=9001)&&(XP <=10000)){
+nivel = "Imortal"
+}else {
+nivel = "Radiante"
+}
+console.log("O heroi de nome " + nome + " está no nivel " + nivel)
